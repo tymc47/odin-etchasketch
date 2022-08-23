cecde1c65e0a37659a45be2aeab5865edb6995d8
Terry Mak <fmak.1536@gmail.com>
2022-08-23 22:11:46 +0800

    add gridsize slider

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-function createGrid(){
+function createGrid(size){
     const panel = document.querySelector('.panel');
 
-    for (let i = 0; i < 256; i++){
+    // clear grid
+    panel.replaceChildren();
+
+    // generate new grid
+    for (let i = 0; i < size**2; i++){
         const gridbox = document.createElement('div');
         gridbox.classList.add("gridbox");
-        gridbox.setAttribute("unselectable", "on");
+        gridbox.style.height = (768/size - 2) +"px";
+        gridbox.style.width = (768/size - 2) +"px";
+
+        gridbox.addEventListener('mouseover', function(){
+            if (isMouseDown){
+            this.style.background = 'black';
+            }
+        })
+
+        gridbox.addEventListener('mousedown', function(){
+            this.style.background = 'black';
+        })
+
         panel.appendChild(gridbox);
     }
 }
 
-
-createGrid()
-const gridboxes = document.querySelectorAll('.gridbox');
-const panel = document.querySelector('.panel');
+createGrid(16);
+const slider = document.querySelector('.slider');
+const slidervalue = document.querySelector('.slidervalue');
 let isMouseDown = false;
+
+slidervalue.textContent = (slider.value + " x " + slider.value);
 
 document.body,onmousedown = () => isMouseDown = true;
 document.body.onmouseup = () => isMouseDown = false;
 
-gridboxes.forEach(gridbox => gridbox.addEventListener('mouseover', function(){
-    if (isMouseDown){
-    this.style.background = 'white';
-    }
+slider.oninput = function(){
+    createGrid(this.value);
+    slidervalue.textContent = (slider.value + " x " + slider.value);
 }
-));
-
-gridboxes.forEach(gridbox => gridbox.addEventListener('mousedown', function(){
-    this.style.background = 'white';
-}
-));
 
