9a926d248322dbae6eb161d8a50a34f2f544edd9
Terry Mak <fmak.1536@gmail.com>
2022-08-24 16:41:40 +0800

    finish javascript

--- script.js
+++ script.js
@@ -8,35 +8,59 @@ function createGrid(size){
     for (let i = 0; i < size**2; i++){
         const gridbox = document.createElement('div');
         gridbox.classList.add("gridbox");
-        gridbox.style.height = (768/size - 2) +"px";
-        gridbox.style.width = (768/size - 2) +"px";
 
         gridbox.addEventListener('mouseover', function(){
             if (isMouseDown){
-            this.style.background = 'black';
+            this.style.background = penColor;
             }
         })
 
         gridbox.addEventListener('mousedown', function(){
-            this.style.background = 'black';
+            this.style.background = penColor;
         })
 
         panel.appendChild(gridbox);
     }
+
+    panel.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    panel.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    slidervalue.textContent = (slider.value + " x " + slider.value);
 }
 
-createGrid(16);
+function clearDrawing() {
+    const gridboxes = document.querySelectorAll('.gridbox');
+    
+    gridboxes.forEach(gridbox => {
+        gridbox.style.background = 'white';
+    })
+}
+
+function changePenColor(){
+    penColor = this.value;
+}
+
+const defaultSize = 16;
+const defaultColor = 'black';
+
 const slider = document.querySelector('.slider');
 const slidervalue = document.querySelector('.slidervalue');
-let isMouseDown = false;
+const buttonClear = document.querySelector('.clear');
+const buttonEraser = document.querySelector('.eraser');
+const buttonPen = document.querySelector('.pen');
+const palette = document.querySelector('.palette');
 
-slidervalue.textContent = (slider.value + " x " + slider.value);
+let penColor = defaultColor;
+let isMouseDown = false;
 
 document.body,onmousedown = () => isMouseDown = true;
 document.body.onmouseup = () => isMouseDown = false;
+slidervalue.textContent = (defaultSize + " x " + defaultSize);
+buttonClear.addEventListener('click', clearDrawing);
+buttonEraser.addEventListener('click', changePenColor);
+buttonPen.addEventListener('click', function(){penColor = palette.value});
+slider.oninput = function(){createGrid(this.value)};
+palette.oninput = changePenColor;
 
-slider.oninput = function(){
-    createGrid(this.value);
-    slidervalue.textContent = (slider.value + " x " + slider.value);
-}
+createGrid(16);
 
